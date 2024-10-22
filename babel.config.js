32086de91bf6ef68ea0f4bc904d091729fb8377e
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-10-22 21:06:32 +0500

    defined architecture and path alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+       'module-resolver',
+       {
+         root: ['./'],
+         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+         alias: {
+           "@src": "./src",
+           "@assets": "./src/assets",
+           "@components": "./src/components",
+           "@redux": "./src/redux",
+           "@screens": "./src/screens",
+            
+         }
+       }
+    ]
+  ]
 };
---
+++ src/components/GifCard.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const GifCard = () => {
+  return (
+    <View>
+      <Text>GifCard</Text>
+    </View>
+  );
+};
+
+export default GifCard;
+
+const styles = StyleSheet.create({});
