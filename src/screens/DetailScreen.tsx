cd129be03808af30e7a750a37c1e79d2cf043cd4
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-10-24 02:47:37 +0500

    Description screen completed

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,14 +1,39 @@
-import {StyleSheet, Text, View} from 'react-native';
+// src/screens/DetailScreen.tsx
+
 import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {RouteProp, useRoute} from '@react-navigation/native';
+import {RootStackParamList} from '@src/constants/types';
+import GifCard from '@src/components/GifCard';
+
+type DetailScreenRouteProp = RouteProp<RootStackParamList, 'Detail'>; // Define the route prop type
 
 const DetailScreen = () => {
+  const route = useRoute<DetailScreenRouteProp>(); // Use the route hook with type safety
+  const {item} = route.params; // Safely extract the item parameter
+
   return (
-    <View>
-      <Text>DetailScreen</Text>
+    <View style={styles.container}>
+      <GifCard
+        image={item.image}
+        rating={item.rating}
+        title={item.title}
+        url={item.url}
+      />
     </View>
   );
 };
 
-export default DetailScreen;
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
 
-const styles = StyleSheet.create({});
+export default DetailScreen;
